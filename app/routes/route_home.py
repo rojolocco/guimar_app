707415645702fa29f6a0151c6a2500afcf8df27d
Christian Garcia <rojolocco@gmail.com>
2024-09-12 12:12:35 -0500

    home router updated

--- app/routes/route_home.py
+++ app/routes/route_home.py
@@ -5,12 +5,10 @@ from fastapi.responses import FileResponse
 from pathlib import Path
 
 STATIC_FILES_DIR = Path(__file__).parent.parent / "static"
-print(STATIC_FILES_DIR)
 
 router_home = APIRouter()
 
 
 @router_home.get("/")
 async def home():
-    # Serve the index.html file from the static files directory
     return FileResponse(STATIC_FILES_DIR / "index.html")
