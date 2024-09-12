6271b149a2a4fddd9e6bb9861c45943cd7b00206
Christian Garcia <rojolocco@gmail.com>
2024-09-12 13:02:05 -0500

    db creation

---
+++ app/database/crud.py
@@ -0,0 +1,21 @@
+# Import libraries
+from dotenv import dotenv_values
+from supabase import create_client, Client
+
+
+config = dotenv_values(".env")
+
+# Create Supabase client
+url: str = config.get("SUPABASE_URL")
+key: str = config.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+# Create CRUD operations for Supabase service
+# Create a function to get all data from a table
+def get_all_data(table: str):
+    return supabase.table(table).select('*').execute()
+
+# Create a function to insert data into a table
+def insert_data(table: str, data: dict):
+    return supabase.table(table).insert(data).execute()
+
